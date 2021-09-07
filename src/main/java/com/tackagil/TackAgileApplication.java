a72f7c7e4b1f260005b4c5d97bc462a77c33e4f2
Elite <rlaeksxp3457@gmail.com>
2021-09-08 01:05:35 +0900

    create 백엔드

---
+++ src/main/java/com/tackagil/TackAgileApplication.java
@@ -0,0 +1,13 @@
+package com.tackagil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TackAgileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TackAgileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tackagil/TackAgileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tackagil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TackAgileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
